5489571a61f9b9e808478da0004d374a389c4c39
STIVVVV <stevencanfield2000@gmail.com>
2022-06-08 17:50:23 -0400

    utilized ejs to make basic blogs

--- app.js
+++ app.js
@@ -10,18 +10,23 @@ app.set('view engine', 'ejs')
 app.listen(3000);
 
 app.get('/', (req, res) => {
-    res.render('index');
+    const blogs = [
+        {title: 'how to get away with murder', snippet: 'its as easy as 1 2 slice'},
+        {title: 'how to commit tax fraud', snippet: 'the irs is actually not that bad'},
+        {title: 'how to make kool-aid', snippet: 'very simple koolaide recipe'}
+    ];
+    res.render('index', {title: 'Home', blogs});
 });
 
 app.get('/about', (req, res) => {
-    res.render('about');
+    res.render('about', {title: 'About'});
 });
 
 app.get('/blogs/create', (req, res) => {
-    res.render('create');
+    res.render('create', {title: 'Create a new Blog'});
 })
 
 //404 page
 app.use((req, res) => {
-    res.status(404).render('404');
+    res.status(404).render('404', {title: '404'});
 });
