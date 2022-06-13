27e2158fd18e88bf1702b71f241e667fc8fc282f
STIVVVV <stevencanfield2000@gmail.com>
2022-06-12 20:00:29 -0400

    blog objs are now shown on the page

--- app.js
+++ app.js
@@ -19,55 +19,25 @@ app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(morgan('dev'));
 
-//mongoose and mongo sandbox routes
-app.get('/add-blog', (req, res) => {
-    const blog = new Blog({
-        title: 'new blog',
-        snippet: 'about my new blog',
-        body: 'more about my new blog'
-    });
-
-    blog.save()
-        .then((result) => {
-            res.send(result)
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-})
-
-app.get('/all-blogs', (req, res) => {
-    Blog.find()
-        .then((result) => {
-            res.send(result);
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-});
-
-app.get('/single-blog', (req, res) =>{
-    Blog.findById('62a677bdcb5ca8e06379ab24')
-        .then((result) => {
-            res.send(result)    
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-})
-
+//routes
 app.get('/', (req, res) => {
-    const blogs = [
-        {title: 'how to get away with murder', snippet: 'its as easy as 1 2 slice'},
-        {title: 'how to commit tax fraud', snippet: 'the irs is actually not that bad'},
-        {title: 'how to make kool-aid', snippet: 'very simple koolaide recipe'}
-    ];
-    res.render('index', {title: 'Home', blogs});
+    res.redirect('/blogs');
 });
 
 app.get('/about', (req, res) => {
     res.render('about', {title: 'About'});
 });
+
+//blog routes
+app.get('/blogs', (req, res) => {
+    Blog.find().sort({ createdAt: -1 })
+        .then((result) => {
+            res.render('index', { title: 'all Blogs', blogs: result })
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+})
 
 app.get('/blogs/create', (req, res) => {
     res.render('create', {title: 'Create a new Blog'});
