e6fd0f4fcb43e95c3cff1772fb40fc379de6981c
STIVVVV <stevencanfield2000@gmail.com>
2022-06-13 13:47:25 -0400

    Set up post requests

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ app.set('view engine', 'ejs')
 
 //middle ware aand static files
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true}));
 app.use(morgan('dev'));
 
 //routes
@@ -33,6 +34,18 @@ app.get('/blogs', (req, res) => {
     Blog.find().sort({ createdAt: -1 })
         .then((result) => {
             res.render('index', { title: 'all Blogs', blogs: result })
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+})
+
+app.post('/blogs', (req, res) => {
+    const blog = new Blog(req.body);
+
+    blog.save()
+        .then((result) => {
+            res.redirect('/blogs');
         })
         .catch((err) => {
             console.log(err);
